0b5fc2e10b1e7fe9bb2230aaee301408827f7e92
miguel <miguelgdpeixoto09@gmail.com>
2026-02-11 12:55:58 -0300

    add security 1.1

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,12 @@
+require("dotenv").config({ path: "./backend/.env" });
 const { Pool } = require("pg");
+
+const isProduction = process.env.NODE_ENV === "production";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === "production"
-    ? { rejectUnauthorized: false }
-    : false,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
 
 module.exports = pool;
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config({ path: "./backend/.env" });
 const express = require("express");
 const cors = require("cors");
 const bcrypt = require("bcrypt");
